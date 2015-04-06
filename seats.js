70a6501e2c11e0031fe814ac813cd71244844358
Viktor_Viktor <life.is.life1992@gmail.com>
2015-04-06 21:53:42 +0300

    documentation added

--- seats.js
+++ seats.js
@@ -24,18 +24,27 @@ function findCat() {
 var selectCat = -1;
 
 function takeCat() {
+    // Если кот выбран - сбросим значение.
     if(selectCat >= 0) {
         selectCat = -1;
         findCat();
     }
+    // Поиск веселых котов из всех
     for ( var i=0; i < cats.length; i++) {
+        // Проверка вселости кота
         if (cats[i]) {
             selectCat = i;
             document.getElementById("c" + i).src = "image/2.png";
             document.getElementById("c" + i).alt = "selected_cat";
             document.getElementById("c" + i).style.border = "2px solid green";
+            // Предлагается клинту выбор, взять ли кота
             var accept = confirm("Кот " + (i + 1) + " - счастливый. Взять этого кота?");
-            if (!accept) {
+            // Если пользователь согласился, кресло бронируется и цикл останавливается
+            if (accept){
+                break;
+            }
+            // Если пользователь отказался, меняем вид кота и продолжаем поиск
+            else{
                 selectCat = -1;
                 document.getElementById("c" + i).src = "image/1.png";
                 document.getElementById("c" + i).alt = "fun_cat";
