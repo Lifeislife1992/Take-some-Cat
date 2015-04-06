ffa11aca1369fb36fbe2c6c090a2e96efa74b86e
Viktor_Viktor <life.is.life1992@gmail.com>
2015-04-06 21:31:52 +0300

    documentation added

--- seats.js
+++ seats.js
@@ -2,26 +2,28 @@
  * Created by Виктор on 04.04.2015.
  */
 alert("Найдите 3-х подряд счастливых котов");
+
+var cats = [false, true, false, true, true, true, false, true, false, false, false, true];
 function findCat() {
-    var cats = [false, true, false, true, true, true, false, true, false, false, false, true];
     for ( var i=0; i < cats.length; i++) {
         if (cats[i]){
-            alert("Кот №" + i + " радуется!");
             document.getElementById("c" + i).src = "image/1.png";
             document.getElementById("c" + i).alt = "fun_cat";
-            // location.reload();
+            document.getElementById("c" + i).style.border = "2px solid blue";
+            alert("Кот №" + i + " радуется!");
         }
         else{
-            alert("Кот №" + i + " грустит...");
             document.getElementById("c" + i).src = "image/3.png";
             document.getElementById("c" + i).alt = "sad_cat";
-            // location.reload();
+            document.getElementById("c" + i).style.border = "2px solid blue";
+            alert("Кот №" + i + " грустит...");
         }
     }
 }
-/*
+
 var selectCat = -1;
-function takeCat(cats) {
+
+function takeCat() {
     if(selectCat >= 0) {
         selectCat = -1;
         findCat();
@@ -31,13 +33,14 @@ function takeCat(cats) {
             selectCat = i;
             document.getElementById("c" + i).src = "image/2.png";
             document.getElementById("c" + i).alt = "selected_cat";
+            document.getElementById("c" + i).style.border = "2px solid green";
             var accept = confirm("Кот " + (i + 1) + " - счастливый. Взять этого кота?");
             if (!accept) {
                 selectCat = -1;
                 document.getElementById("c" + i).src = "image/1.png";
                 document.getElementById("c" + i).alt = "fun_cat";
+                document.getElementById("c" + i).style.border = "2px solid red";
             }
         }
     }
 }
-*/
